e481b332c3bf0a9a5e9d6e8134290f0c50add6cd
KingUnknown15 <64892155+KingUnknown15@users.noreply.github.com>
2023-07-26 17:13:03 +0700

    Tested API connection

---
+++ apitest.py
@@ -0,0 +1,11 @@
+from username import username, email, password
+import requests
+from datetime import datetime
+
+web_url = "https://api.mangadex.org"
+r = requests.get(
+    f"{web_url}/manga",
+    params={"title": "One Piece"}
+)
+print(r.json())
+print([manga["id"] for manga in r.json()["data"]])
